aa7c146ec0685d47d29570758fcdf8ac3a29c535
sebastian-lee <sebastian.lee.9@gmail.com>
2017-11-16 10:13:30 -0500

    One more time

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client/build')));
-app.use('/', index);
+//app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
